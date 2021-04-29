ab34bd0830a108b3d8ddd015bb2de1392a165731
watchbutdonotlearn <71526591+watchbutdonotlearn@users.noreply.github.com>
2021-04-29 13:19:39 -0500

    Add storage of GPA values

--- background.js
+++ background.js
@@ -26,8 +26,7 @@ window.onload=function(){
     document.getElementById('popupBtn').addEventListener('click', showhide);
 	document.getElementById('closeWeight').addEventListener('click', showhide);
 	
-	// START GETELEMENTBYIDing EVERYTHING NOW LMAO I HATE LIFE
-	
+	document.getElementById('closeWeight').addEventListener('click', getGPAValues);
 	
 }
 function showhide(){
@@ -37,6 +36,28 @@ function showhide(){
 }
 
 function getGPAValues(){
+	//begin saving the GPA weights to variables
+	var gpa1 = document.forms.GPAform1.GPA.value;
+	console.log(gpa1);
+	var gpa2 = document.forms.GPAform2.GPA.value;
+	console.log(gpa2);
+	var gpa3 = document.forms.GPAform3.GPA.value;
+	console.log(gpa3);
+	var gpa4 = document.forms.GPAform4.GPA.value;
+	console.log(gpa4);
+	var gpa5 = document.forms.GPAform5.GPA.value;
+	console.log(gpa5);
+	var gpa6 = document.forms.GPAform6.GPA.value;
+	console.log(gpa6);
+	var gpa7 = document.forms.GPAform7.GPA.value;
+	console.log(gpa7);
 	
-	
+	// begin storing variables in local storage
+	chrome.storage.local.set({storedGPA1: gpa1})
+	chrome.storage.local.set({storedGPA2: gpa2})
+	chrome.storage.local.set({storedGPA3: gpa3})
+	chrome.storage.local.set({storedGPA4: gpa4})
+	chrome.storage.local.set({storedGPA5: gpa5})
+	chrome.storage.local.set({storedGPA6: gpa6})
+	chrome.storage.local.set({storedGPA7: gpa7})
 }
